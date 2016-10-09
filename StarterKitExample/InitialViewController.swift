0c7f10c9b6e6c721768271193f5733d5b8ac5740
Ryan Pliske <ryan.pliske@monsanto.com>
2016-10-09 18:50:51 -0500

    Initial Commit

---
+++ StarterKitExample/InitialViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InitialViewController.swift
+//  StarterKitExample
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 10/9/16.
+//  Copyright © 2016 competidev. All rights reserved.
+//
+
+import Foundation
