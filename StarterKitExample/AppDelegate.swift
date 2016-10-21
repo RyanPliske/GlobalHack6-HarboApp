eb64b41bdbe6665a906bf48b1ae8943cde9d2c67
Ryan Pliske <ryan.pliske@monsanto.com>
2016-10-20 19:52:49 -0500

    we need these

--- StarterKitExample/AppDelegate.swift
+++ StarterKitExample/AppDelegate.swift
@@ -5,11 +5,12 @@ import StarterKit
 class AppDelegate: UIResponder, UIApplicationDelegate, StarterKitDelegate {
 
     var window: UIWindow?
-    var starterKitInterface: StarterKit!
+    var starter: Starter!
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        let appInfo = AppInfo(loginBackground: UIImage(named: "login1")!)
-        starterKitInterface = StarterKit(window: &window, initialViewController: InitialViewController(), delegate: self, appInfo: appInfo)
+        let fbInfo = FacebookInfo(facebookAppId: "580752305447916", facebookAppDisplayName: "StarterKit")
+        let appInfo = AppInfo(loginBackground: UIImage(named: "login1")!, facebookInfo: fbInfo)
+        self.starter = Starter(window: &window, initialViewController: InitialViewController(), delegate: self, appInfo: appInfo)
         return true
     }
     
